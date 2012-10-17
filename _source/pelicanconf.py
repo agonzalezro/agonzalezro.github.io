3515760ee8c92f51c20b7404966d74d9c3dfec53
Álex González <agonzalezro@gmail.com>
2012-10-18 01:57:24 +0200

    Proof of concept finished

--- _source/pelicanconf.py
+++ _source/pelicanconf.py
@@ -1,65 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-AUTHOR = u'Álex González'
+AUTHOR = u'Álex'
 SITENAME = 'Ehyyy, non hai fallo!'
-SITEURL = 'http://example.com'
+SITEURL = 'http://agonzalezro.github.com'
 
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = 'en'
 
-# Blogroll
-LINKS =  (('Paylogic', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Google', 'http://python.org'),
-          ('Shit', '#'),
-          ('Shit1', '#'),
-          ('Shit2', '#'))
-
-# Social widget
-SOCIAL = (('Twitter', '#'),
-          ('Facebook', '#'),
-          ('Google+', '#'))
-
-DEFAULT_PAGINATION = 3
-
-GITHUB_ACTIVITY_FEED = 'https://github.com/agonzalezro.atom'
-GITHUB_URL = 'http://agonzalezro.github.com/'
-LICENSE = 'SHITTY GPL'
-
-
-PLUGINS = ['pelican.plugins.github_activity',
-           'pelican.plugins.gravatar',
-           'pelican.plugins.global_license']
-
-TAG_CLOUD_STEPS = 4
-TAG_CLOUD_STEPSG_CLOUD_MAX_ITEMS = 100
-
-MENUITEMS = SOCIAL
+DEFAULT_PAGINATION = 2
 
 TWITTER_USERNAME = 'agonzalezro'
-
-# global metadata to all the contents
-DEFAULT_METADATA = (('yeah', 'it is'),)
 
 # static paths will be copied under the same name
 STATIC_PATHS = ["pictures", ]
 
-# A list of files to copy from the source to the destination
-FILES_TO_COPY = (('extra/robots.txt', 'robots.txt'),)
-
 DISQUS_SITENAME = 'agonzalezro'
 
-##THEME = 'simple'
-##THEME = 'brownstone'
-##THEME = 'mnmlist'
-
-THEME = '_dev_theme'
-#THEME = 'notmyidea-cms'
-#THEME = 'subtle'
-
-#interesante# THEME = 'tuxlite_tbs'  # parecido a bootstrap
-#THEME = 'dev-random'  # bastante parecido a lo que quiero
+THEME = '_journal'
 
 OUTPUT_PATH = '.'
 DELETE_OUTPUT_DIRECTORY = False
--- _source/publishconf.py
+++ _source/publishconf.py
@@ -8,11 +8,3 @@ from pelicanconf import *
 SITEURL = ''
 
 DELETE_OUTPUT_DIRECTORY = True
-
-# Following items are often useful when publishing
-
-# Uncomment following line for absolute URLs in production:
-#RELATIVE_URLS = False
-
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
